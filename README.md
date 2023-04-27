3fd04e81279e41d40636b750a0424f4b764818d3
Ashleigh Chute <ashlhc@uw.edu>
2023-04-26 19:34:06 -0700

    customized css and refined code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Alhc-Team-Profile-Generator
-Team Profile Generator 
+
+## Description
+    This Team Profile Generator is meant to create an HTML page for a development team. You can add Manager's, Engineer's, and Intern's. Each type of employee has specific data pertaining to whether they are a Manager, Engineer, or Intern. You use Inquirer to collect this specific information and it outputs a readable HTML file. 
+
+## Installation
+    To use this Team Profile Generator you'll need to clone this repository, install node.js, and run npm install in the integrated terminal.
+
+## Usage
+    A demonstration of my generator can be found [here]()
+
+
+## Credits
+    Starter code was provided
+    Inquirer
+    Jest NPM 
--- index.js
+++ index.js
@@ -9,20 +9,19 @@ const generateHtml = require('./util/generateHtml');
 const employees = [];
 
 function promptEmployee() {
-    inquirer
-        .prompt([
+    inquirer.prompt([
             {
                 type:'confirm',
                 name: 'addEmployee',
                 message: 'Do you want to add an Employee?',
             },
         ])
-        .then((answer) => {
-            if(answer.addEmployee) {
+        .then((answers)=>{
+            if(answers.addEmployee) {
                 promptEmployeeType();
             } else {
-                fs.writeFile('index.html', generateHtml(employees), (err,data) => {
-                    if (err) {
+                fs.writeFile('index.html', generateHtml(employees), (err,data)=>{
+                    if(err) {
                         throw err;
                     }
                     console.log('Yay!');
@@ -30,22 +29,21 @@ function promptEmployee() {
                 
             }
         })
-        .catch((error) => {
+        .catch((error)=>{
             console.log('Error:', error);
         });
 }
 function promptEmployeeType() {
-    inquirer
-        .prompt([
+    inquirer.prompt([
             {
                 type: 'list',
                 name: 'employeeType',
                 message: 'Select employee type:',
-                choiches: ['Manager', 'Engineer', 'Intern'],
+                choices: ['Manager', 'Engineer', 'Intern'],
             },
         ])
-        .then((answer) => {
-            switch (answer.employeeType) {
+        .then((answers)=>{
+            switch (answers.employeeType) {
                 case 'Manager':
                     promptManager();
                     break;
@@ -60,41 +58,112 @@ function promptEmployeeType() {
                         promptEmployeeType();
             }
         })
-        .catch((error) => {
+        .catch((error)=>{
             console.log('Error:', error);
         });
 }
 function promptManager() {
-    inquirer
-        .prompt([
+    inquirer.prompt([
             {
                 type: 'input',
                 name: 'name',
                 message: "Enter manager's name:",
-            },
-            {
+            },{
                 type: 'input',
                 name: 'id',
                 message: "Enter manager's ID:",
-            },
-            {
+            },{
                 type: 'input',
                 name: 'email',
                 message: "Enter manager's email address:",
-            },
-            {
+            },{
                 type: 'input',
                 name: 'officeNumber',
                 message: "Enter manager's office number:",
             },
         ])
-        .then((answers) => {
+        .then((answers)=>{ 
             const manager = new Manager(
                 answers.name,
                 answers.id,
                 answers.email,
                 answers.officeNumber
             );
+            employees.push(manager);
+            promptEmployee();
         })
+        .catch((error)=>{
+            console.log('Error:',error);
+        });
 }
+
+function promptEngineer() {
+    inquirer.prompt([
+        {
+            type:'input',
+            name: 'name',
+            message:"Enter engineer's name",
+        },{
+            type: 'input',
+            name: 'id',
+            message: "Enter engineer's ID",
+        },{
+            type: 'input',
+            name: 'email',
+            message: "Enter engineer's email address:",
+        },{
+            type: 'input',
+            name: 'github',
+            message: "Enter engineer's Github",
+        },
+    ])
+    .then((answers)=>{
+        const engineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.github
+        );
+        employees.push(engineer);
+        promptEmployee();
+    })
+    .catch((error)=>{
+        console.log('Error:', error);
+    });
+}
+function promptIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter intern's name"
+        },{
+            type: 'input',
+            name: 'id',
+            message: "Enter intern's ID"
+        },{
+            type: 'input',
+            name: 'email',
+            message: "Enter intern's email adress",
+        },{
+            type: 'input',
+            name: 'school',
+            message: "Enter name of intern's school",
+        },
+    ])
+    .then((answers)=>{
+        const intern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.school
+        );
+        employees.push(intern);
+        promptEmployee();
+    })
+    .catch((error)=>{
+        console.log('Error', error);
+    });
+}
+promptEmployee();
 
